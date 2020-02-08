7de58d843dae07fb46ae46f1fedd8ecc244a247f
Brittney Cortez <brittneycortez@Brittneys-MacBook-Pro.local>
2020-02-07 18:44:50 -0800

    made a way for user to enter numbers and operation

--- calc.rb
+++ calc.rb
@@ -1,6 +1,20 @@
-puts "Welcome to my Calculator!"
+# Welcome to the Crapulator
+puts "Welcome to the Crappily Rendered Arithmetic Program Calculator..."
+puts "The CRAPulator for short."
 puts "++==++==++==++==++==++==++=="
+puts "==++==++==++==++==++==++==++"
+
 puts "Enter a number!"
+inp1 = gets.chomp.to_i
+
+puts "Enter one of the follow operations: "
+puts " + - * /"
+op = gets.chomp
+
+puts "Enter a number!"
+inp2 = gets.chomp.to_i
+
+puts inp1.to_s + op.to_s + inp2.to_s
 
 
 
